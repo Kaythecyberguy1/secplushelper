fe7277276a9172da04ae00f3b5733d4a3789d89a
Kaythecyberguy1 <95498182+Kaythecyberguy1@users.noreply.github.com>
2025-05-04 19:16:19 -0700

    come

--- js/main.js
+++ js/main.js
@@ -233,6 +233,16 @@ const gameState = {
     }
 };
 
+
+const acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    panel.style.display = panel.style.display === "block" ? "none" : "block";
+  });
+}
+
 // Achievement definitions
 const achievements = {
     firstQuestion: {
